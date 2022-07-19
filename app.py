6e399eb518b643574eda7a284dcf4c5ce29ee252
AnhVuong146 <99466654+AnhVuong146@users.noreply.github.com>
2022-07-18 17:50:50 -0700

    updated app.py
    added a page for task to do list

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 #These are for reading from the databases
@@ -32,6 +33,12 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    
+    
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if not session.get('logged_in'):
@@ -82,6 +89,37 @@ def logout():
     session['logged_in'] = False
     return redirect(url_for('home'))
    
+
+@app.route("/task")
+def task():
+    todo_list = Todo.query.all()
+    return render_template("task.html", todo_list=todo_list)
+ 
+ 
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("task"))
+
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("task"))
+
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("task"))
+ 
 if __name__ == '__main__':
     app.debug = True
     db.create_all()
